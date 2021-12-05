5c588762e48f607519fcffc899aadc23643d6c36
Aedriane <yamcha@yamcha.local>
2021-12-05 14:11:08 +1100

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './g.jpg';
 import './App.css';
 
 function App() {
@@ -6,17 +6,64 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+        <p className="intro">
+          Hey, I'm <b className="white">Aedriane.</b>
+        <br />
+          IT Engineer.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p className="aboutme">
+        About me.
+        </p>
+        <p className="aboutmepara">
+          I'm a dude who lives and breathes tech. Present me with a problem, and I will bounce back with a solution (99.98% of the time). 
+          I'm a huge music enthusiast, and play some mad guitar in my leisure and hopelessly attempt to produce music.<br />
+          I am currently based in Melbourne, Australia.
+        </p>
+
+        <p className="work">
+          Work.
+        </p>
+        <p className="workpara">         
+          Currently, I am a Desktop Lead Engineer for Securelogic Solutions, an MSP based in Melbourne, Australia. 
+          If you're keen to know more, take a peak at my resume <a href="https://drive.google.com/file/d/12ywt35tTCMybG9vjsG_ZT6OEuMVThlP2/view?usp=sharing" target="_blank" className="white">here.</a>
+        </p>
+
+        <p className="interests">
+          Interests.
+        </p>
+        <p className="interestspara">  
+          Take a look at my interests below, if it interests you that is.  
+
+        <br /><br />     
+
+          <li>Crypto & NFTs.</li>
+          <li>Cloud Tech.</li>
+          <li>Automation.</li>
+          <li>Music.</li>
+          <li>Cars.</li>
+        </p>
+
+        <p className="projects">
+          Interests.
+        </p>
+        <p className="projectspara">       
+          Watch this space, bruh. 👀 
+        </p> 
+
+        <p className="contact">
+          Contact.
+        </p>
+        <p className="contactpara">       
+          Kindly shoot me an <a href="mailto:aedrianehernan@gmail.com" className="white">email</a> if you're keen to have a chat.
+          If you're not huge on electronic mail, feel free to drop by via <a href="https://www.linkedin.com/in/aedriane/" target="_blank" className="white">LinkedIn.</a>
+        </p> 
+
+        <p className="footer">
+          Copyright © 2021 aedriane.tech
+        </p>
+
       </header>
     </div>
   );
